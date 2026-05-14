87547c6dbc2a0b5ac2300498d80d09e0fbbc106e
Armando051 <armandopaco051@gmail.com>
2026-05-14 14:54:56 -0400

    despliegue

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: 'http://127.0.0.1:8000/api/',
+ //baseURL: 'http://127.0.0.1:8000/api/',
   //baseURL: 'https://backend-barberia-ohjh.onrender.com/api',
+  baseURL : 'https://backendbarber-copia.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
